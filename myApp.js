48933312f42d4d9254557c0925bdde14481f992a
sikandar <sikndar.iqbal@gmail.com>
2018-05-31 08:16:37 -0700

    'pug'

--- myApp.js
+++ myApp.js
@@ -1,19 +1,34 @@
+//import dependies
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var app = express();
+var upload = multer();
 
+//get request rendering form (PUG)
+app.get('/', (req,res) => {
 
+    res.render('form');
 
-app.set('view engine', 'pug');
+});
+
+//setting engine to pug
+app.set('view engine','pug');
 app.set('views', './views');
 
-app.get('/', (req,res) =>{
-    
-    res.render('first_view', {
-        name : 'sikiii',
-        desc : 'verynice'
-    });
+//form data parsing in JSON
+app.use(bodyParser.json());
 
-} );
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
 
-app.listen(3000);
+app.use(upload.array());
+app.use(express.static('public'));
+
+app.post('/',(req,res) => {
+    console.log(req.body);
+    res.send("recieved your request!");
+ });
+ app.listen(3000);
