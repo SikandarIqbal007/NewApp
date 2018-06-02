2ce58e14625a824e7d1f878410691a79705c96be
SikandarIqbal007 <32866763+SikandarIqbal007@users.noreply.github.com>
2018-06-02 11:37:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-var express = require('express');
+console.log('shit');
+ var express = require('express');
 var bodyParser = require('body-parser');
-var multer = require('multer');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var jade = require('jade');
+ var multer = require('multer');
+// var cookieParser = require('cookie-parser');
+// var session = require('express-session');
+// var jade = require('jade');
 
-var upload = multer();
-var app = express();
-
-
-app.set('view engine', 'pug');
-
-app.set('views', './views');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(upload.array());
-
-app.use(cookieParser());
-app.use(session({
-    secret: 'shit',
-    name: 'shit',
-    // connect-mongo session store
-    proxy: true,
-    resave: true,
-    saveUninitialized: true
-}));
+// var upload = multer();
+ var app = express();
+app.get('/', function(req, res){
+    res.send('hello world');
+});
 
 
-var routers = require('./routes/routers.js');
+// app.set('view engine', 'pug');
 
-app.use('/',routers);
+// app.set('views', './views');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+// app.use(upload.array());
+
+// app.use(cookieParser());
+// app.use(session({
+//     secret: 'shit',
+//     name: 'shit',
+//     // connect-mongo session store
+//     proxy: true,
+//     resave: true,
+//     saveUninitialized: true
+// }));
 
 
-app.listen(3000);
+// var routers = require('./routes/routers.js');
+
+// app.use('/',routers);
+
+
+// app.listen(3000);
