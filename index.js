9369a1eae12229915c222bc138456981ac9b79b4
SikandarIqbal007 <32866763+SikandarIqbal007@users.noreply.github.com>
2018-06-02 14:30:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var bodyParser = require('body-parser');
 
 // var upload = multer();
  var app = express();
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 // app.get('/', function(req, res){
 //     res.send('hello world');
 // });
