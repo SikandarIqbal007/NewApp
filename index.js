0ab6403dda2b3b550e30004e94156009b5e1c6f2
SikandarIqbal007 <32866763+SikandarIqbal007@users.noreply.github.com>
2018-06-03 02:27:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
+// express()
  
 
-  .get('/', (req, res) => res.send('DONE'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//   .get('/', (req, res) => res.send('DONE'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
 
-//  var express = require('express');
+ var express = require('express');
 
-// // var cookieParser = require('cookie-parser');
-// // var session = require('express-session');
-// // var jade = require('jade');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var jade = require('jade');
 
-// // var upload = multer();
-//  var app = express();
+var upload = multer();
+ var app = express();
 
 // app.listen(process.env.PORT || 5000, function(){
 //   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
@@ -30,31 +30,31 @@ express()
 // });
 
 
-// app.set('view engine', 'pug');
+app.set('view engine', 'pug');
 
-// app.set('views', './views');
+app.set('views', './views');
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-// app.use(upload.array());
+app.use(upload.array());
 
-// app.use(cookieParser());
-// app.use(session({
-//     secret: 'shit',
-//     name: 'shit',
-//     // connect-mongo session store
-//     proxy: true,
-//     resave: true,
-//     saveUninitialized: true
-// }));
-
-
-// var routers = require('./routes/routers.js');
-
-// app.use('/',routers);
+app.use(cookieParser());
+app.use(session({
+    secret: 'shit',
+    name: 'shit',
+    // connect-mongo session store
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
 
 
-// app.listen(3000);
+var routers = require('./routes/routers.js');
+
+app.use('/',routers);
+
+
+app.listen(3000);
