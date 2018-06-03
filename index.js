8a90afc4ad2d7f5e883e805e239aa2f1982f4d0c
sikandar <sikndar.iqbal@gmail.com>
2018-06-03 06:36:04 -0700

    'c'

--- index.js
+++ index.js
@@ -12,51 +12,67 @@
 
 
 
- var express = require('express');
 
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var multer = require('multer');
-var bodyParser = require('body-parser');
-var jade = require('jade');
 
-var upload = multer();
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
 var app = express();
 
-// app.listen(process.env.PORT || 5000, function(){
-//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
-
-// app.get('/', function(req, res){
-//     res.send('hello world');
-// });
 
 
-app.set('view engine', 'pug');
+app.get('/', function(req,res){
+res.send('I want a pizza right now!');
+});
 
-app.set('views', './views');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(upload.array());
-
-app.use(cookieParser());
-app.use(session({
-    secret: 'shit',
-    name: 'shit',
-    // connect-mongo session store
-    proxy: true,
-    resave: true,
-    saveUninitialized: true
-}));
+app.listen(PORT);
 
 
-var routers = require('./routes/routers.js');
+//  var express = require('express');
 
-app.use('/',routers);
+// var cookieParser = require('cookie-parser');
+// var session = require('express-session');
+// var multer = require('multer');
+// var bodyParser = require('body-parser');
+// var jade = require('jade');
+
+// var upload = multer();
+// var app = express();
+
+// // app.listen(process.env.PORT || 5000, function(){
+// //   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// // });
+
+// // app.get('/', function(req, res){
+// //     res.send('hello world');
+// // });
 
 
-app.listen(3000);
+// app.set('view engine', 'pug');
+
+// app.set('views', './views');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+// app.use(upload.array());
+
+// app.use(cookieParser());
+// app.use(session({
+//     secret: 'shit',
+//     name: 'shit',
+//     // connect-mongo session store
+//     proxy: true,
+//     resave: true,
+//     saveUninitialized: true
+// }));
+
+
+// var routers = require('./routes/routers.js');
+
+// app.use('/',routers);
+
+
+// app.listen(3000);
